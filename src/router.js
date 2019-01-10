831e931983c3fa51a5e705e3d19ff8aa302416c9
Robert Buchberger <rbuchberger@gmail.com>
2019-01-10 21:03:01 +0100

    Lesson 4 complete

--- src/router.js
+++ src/router.js
@@ -1,26 +1,31 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import EventCreate from './views/EventCreate.vue'
+import EventList from './views/EventList.vue'
+import EventShow from './views/EventShow.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'event-list',
+      component: EventList
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: '/event',
+      name: 'event-show',
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+        import(/* webpackChunkName: "about" */ './views/EventShow.vue')
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/EventCreate.vue')
+    },
   ]
-});
+})
---
+++ src/views/EventShow.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Show Event</h1>
+</template>
