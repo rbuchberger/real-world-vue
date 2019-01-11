94330f2b987f03d74071f8a436a48cc9e9a4da9a
Robert Buchberger <rbuchberger@gmail.com>
2019-01-11 14:40:44 +0100

    Lesson 7 complete

--- src/main.js
+++ src/main.js
@@ -2,6 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+// The following finds all files in ./components/ which begin with Base
+const requireComponent = require.context(
+  './components', // Directory to search
+  false, // Search subdirectories?
+  /Base[A-Z]\w+\.(vue|js)$/ // Filename regex
+)
+
+// Requires each component found by require.context
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  // Formats names from filenames
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 Vue.config.productionTip = false
 
